886e13b6322264fdc09f2b48fa8fa273fb939f46
Arendarski Michał <arendarskimichal1@gmail.com>
2019-05-20 21:12:10 +0200

    Add PomodoroHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
+    <PomodoroHeader/>
   </div>
 </template>
 
 <script>
+import PomodoroHeader from './components/PomodoroHeader';
+
 export default {
   name: 'app',
+  components: {
+    PomodoroHeader
+  }
 }
 </script>
---
+++ src/components/PomodoroHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="PomodoroHeader">
+    <img alt="Vue logo" src="../assets/logo.png" class="PomodoroHeader__img">
+    <h1 class="PomodoroHeader__heading">Vue Pomodoro</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'PomodoroHeader',
+}
+</script>
+
+<style lang="scss">
+@import '../assets/styles/partials/_mixins';
+
+.PomodoroHeader {
+  align-items: center;
+  display: flex;
+
+  &__heading {
+    display: none;
+    margin: 0;
+    text-align: center;
+
+    @include respond-to('tablet-min') {
+      display: block;
+    }
+  }
+
+  &__img {
+    height: 50px;
+    width: 50px;
+  }
+}
+</style>
