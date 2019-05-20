e0daf3ba420770c0fc39491a80dc32ea49f00079
Arendarski Michał <arendarskimichal1@gmail.com>
2019-05-20 18:15:56 +0200

    Add global styles for the application

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+/*
+  Apply global styles for the application
+*/
+
+require('./assets/styles/index.scss');
+
 Vue.config.productionTip = false;
 
 new Vue({
