608bcebc5c8fcadaa0a142ce05e2970c576a0d25
Arendarski Michał <arendarskimichal1@gmail.com>
2019-05-21 14:51:15 +0200

    Add config file

---
+++ config/index.js
@@ -0,0 +1,11 @@
+export default Object.freeze({
+  workTimeInSeconds: 1500,
+  breakTimeInSeconds: 300,
+  longBreakTimeInSeconds: 300 * 2,
+  breaksUntilLongBreak: 5,
+  titles: {
+    break: 'Break',
+    longBreak: 'Long Break',
+    work: 'Work'
+  }
+});
